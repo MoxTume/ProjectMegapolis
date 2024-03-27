8bf29a9829296473c6e30ce47b0aef696d176851
MEGAPOLIS <funder340@gmail.com>
2024-03-27 16:03:55 +0300

    Первое задание

--- Первое задание.py
+++ Первое задание.py
@@ -1,5 +1,20 @@
 import csv
 with open('history_mirror.csv', encoding='utf8') as file:
     reader = list(csv.DictReader(file, delimiter=','))[1:]
+    mn_age = 10000
+    pred = []
     for row in reader:
-        print(f"{row['date']}, {row['username']}, {row['verdict']}")
+        if row['verdict'] == 'Победа над смертью':
+            surname, name, otch = row['username'].split(' ')
+            age = row['date'].split('-')[0]
+            pred.append(f"{row['date']}, {row['username']}")
+            if int(age) < mn_age:
+                mn_age = int(age)
+                mn_date = row['date']
+                mn_fullname = f"{surname} {name[0]}.{otch[0]}"
+    print(f"Сообщение было зафиксировано: {mn_date} у пользователя {mn_fullname}")
+with open('mirror_error.csv', 'w', encoding='utf8', newline='') as file:
+    writer = csv.writer(file, delimiter=',')
+    writer.writerow(['date', 'username'])
+    for i in pred:
+        writer.writerows([i.split(',')])
